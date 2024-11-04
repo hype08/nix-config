3a70dd027abd4ad8c49dba35c3c1e43d8b94b228
hype08 <henry.zhang@hey.com>
2024-11-03 18:10:09 -0800

    add home-manager

--- flake.nix
+++ flake.nix
@@ -3,37 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }@inputs: 
   let
-    host = "my-nixos";
+    host = "default";
     username = "henry";
   in
   {
     # Please replace my-nixos with your hostname
-    nixosConfigurations = {
-      "${host}" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-	  ./config.nix
+	  ./hosts/default/config.nix
 	  nixos-hardware.nixosModules.lenovo-thinkpad-t480s
-          home-manager.nixosModules.home-manager
-          {
-	    home-manager.extraSpecialArgs = {
-	      inherit username;
-	      inherit inputs;
-	      inherit host;
-	    };
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = import ./home.nix;
-          }
+          inputs.home-manager.nixosModules.default
        ];
-      };
     };
   };
 }
