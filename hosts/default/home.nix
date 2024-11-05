ae3d4d7ca0eb40a0bab40e991dfb8c8d77bac485
hype08 <henry.zhang@hey.com>
2024-11-04 22:45:24 -0800

    add few shell aliases

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -13,7 +13,19 @@ in
   home.homeDirectory = "/home/henry";
   home.stateVersion = "23.11";
 
-  # Install & Configure Git
+  programs.bash = {
+      enable = true;
+      enableCompletion = true;
+      shellAliases = {
+	g = "lazygit";
+	rebuild = "sudo nixos-rebuild switch --flake /home/henry/Documents/nix-config/#default --show-trace";
+        ".." = "cd ..";
+        ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+        sv = "sudo nvim";
+        v = "nvim";
+      };
+    };
+
   programs.git = {
     enable = true;
     userName = "${gitUsername}";
