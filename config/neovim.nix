4e2fed263bacf4a2f96b747c5f39bc28bae7be00
hype08 <henry.zhang@hey.com>
2024-11-07 20:40:12 -0800

    add some starter plugins

--- config/neovim.nix
+++ config/neovim.nix
@@ -1,16 +1,58 @@
+{ pkgs, inputs, ... }:
 {
-  pkgs,
-  ...
-}: {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      telescope-nvim
-      telescope-fzf-native-nvim
-    ];
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      withNodeJs = true;
+      extraPackages = with pkgs; [
+        lua-language-server
+        gopls
+        xclip
+        wl-clipboard
+        luajitPackages.lua-lsp
+        nil
+        rust-analyzer
+        #nodePackages.bash-language-server
+        yaml-language-server
+        pyright
+        marksman
+      ];
+      plugins = with pkgs.vimPlugins; [
+        alpha-nvim
+        auto-session
+        bufferline-nvim
+        dressing-nvim
+        indent-blankline-nvim
+        nui-nvim
+        nvim-treesitter.withAllGrammars
+        lualine-nvim
+        nvim-autopairs
+        nvim-web-devicons
+        nvim-cmp
+        nvim-surround
+        nvim-lspconfig
+        cmp-nvim-lsp
+        cmp-buffer
+        luasnip
+        cmp_luasnip
+        friendly-snippets
+        lspkind-nvim
+        comment-nvim
+        nvim-ts-context-commentstring
+        plenary-nvim
+        neodev-nvim
+        luasnip
+        telescope-nvim
+        todo-comments-nvim
+        nvim-tree-lua
+        telescope-fzf-native-nvim
+        vim-tmux-navigator
+      ];
+    };
   };
 }
+
