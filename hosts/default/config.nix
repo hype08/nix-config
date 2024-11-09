c148f2cb8a70c53c5d6a9384a8123eb8077d1801
hype08 <henry.zhang@hey.com>
2024-11-08 20:35:00 -0800

    dedupe pkgs and sort

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -104,16 +104,14 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
     curl
-    dunst #notifications
+    dunst # Notifications
     git
     lazygit
     neovim
     ripgrep
-    swww # Wallpapers
     spotify
+    swww # Wallpapers
     vim
     (waybar.overrideAttrs (oldAttrs: {
         mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
