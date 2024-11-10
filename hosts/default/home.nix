3e717e84263ce52bcb7037e40c2024b817fd3ec7
hype08 <henry.zhang@hey.com>
2024-11-10 12:34:36 -0800

    s

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -26,6 +26,7 @@ in
         ".." = "cd ..";
         config = "nvim $HOME/Documents/nix-config";
         ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+        rb = "sudo nixos-rebuild switch --flake $HOME/nix-config/#default --show-trace";
         sv = "sudo nvim";
         v = "nvim";
       };
