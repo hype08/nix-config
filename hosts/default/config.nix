0f788a1c81d993be498816ad4edbcc49b66c5a20
hype08 <henry.zhang@hey.com>
2024-11-11 10:27:36 -0800

    cleanup config

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -1,23 +1,134 @@
-
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 let
   inherit (import ./variables.nix) keyboardLayout;
 in
 {
-  imports =
-    [ 
-      ./hardware.nix
-      ./users.nix
-    ];
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
-  # Styling Options
+  environment = {
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
+    };
+    systemPackages = with pkgs; [
+      curl
+      dunst
+      git
+      hyprpaper
+      kitty
+      lazygit
+      neovim
+      networkmanagerapplet
+      ripgrep
+      rofi-wayland
+      spotify
+      vim
+      wget
+      whatsapp-for-linux
+      zoxide
+      (waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      }))
+    ];
+  };
+
+  fonts.packages = with pkgs; [
+    font-awesome
+    material-icons
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+  ];
+
+  hardware.pulseaudio.enable = false;
+
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  imports = [ 
+    ./hardware.nix
+    ./users.nix
+  ];
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs = {
+    hyprland.enable = true;
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = false;
+        buf.symbol = " ";
+        c.symbol = " ";
+        directory.read_only = " 󰌾";
+        docker_context.symbol = " ";
+        fossil_branch.symbol = " ";
+        git_branch.symbol = " ";
+        golang.symbol = " ";
+        hg_branch.symbol = " ";
+        hostname.ssh_symbol = " ";
+        lua.symbol = " ";
+        memory_usage.symbol = "󰍛 ";
+        meson.symbol = "󰔷 ";
+        nim.symbol = "󰆥 ";
+        nix_shell.symbol = " ";
+        nodejs.symbol = " ";
+        ocaml.symbol = " ";
+        package.symbol = "󰏗 ";
+        python.symbol = " ";
+        rust.symbol = " ";
+        swift.symbol = " ";
+        zig.symbol = " ";
+      };
+    };
+  };
+
+  security.rtkit.enable = true;
+
+  services = {
+    pipewire = {
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      enable = true;
+      jack.enable = true;
+      pulse.enable = true;
+    };
+    printing.enable = true;
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+  };
+
   stylix = {
-    enable = false;
-    image = ../../config/wallpapers/wallpaper.jpg;
     base16Scheme = {
       base00 = "232136";
       base01 = "2a273f";
@@ -36,244 +147,43 @@ in
       base0E = "f6c177";
       base0F = "56526e";
     };
-    polarity = "dark";
-    opacity.terminal = 0.8;
-    cursor.package = pkgs.bibata-cursors;
-    cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 24;
+    cursor = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+    enable = false;
     fonts = {
       monospace = {
-        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
         name = "JetBrainsMono Nerd Font Mono";
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
       };
       sansSerif = {
-        package = pkgs.montserrat;
         name = "Montserrat";
+        package = pkgs.montserrat;
       };
       serif = {
-        package = pkgs.montserrat;
         name = "Montserrat";
+        package = pkgs.montserrat;
       };
       sizes = {
         applications = 12;
-        terminal = 15;
         desktop = 11;
         popups = 12;
+        terminal = 15;
       };
     };
+    image = ../../config/wallpapers/wallpaper.jpg;
+    opacity.terminal = 0.8;
+    polarity = "dark";
   };
 
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts-emoji
-      noto-fonts-cjk-sans
-      font-awesome
-      # Commenting Symbola out to fix install this will need to be fixed or an alternative found.
-      # symbola
-      material-icons
-    ];
-  };
+  system.stateVersion = "24.05";
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "America/Vancouver";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_CA.UTF-8";
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
+  xdg.portal = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  programs.hyprland.enable = true;
-
-  programs = {
-    starship = {
-      enable = true;
-      settings = {
-        add_newline = false;
-        buf = {
-          symbol = " ";
-        };
-        c = {
-          symbol = " ";
-        };
-        directory = {
-          read_only = " 󰌾";
-        };
-        docker_context = {
-          symbol = " ";
-        };
-        fossil_branch = {
-          symbol = " ";
-        };
-        git_branch = {
-          symbol = " ";
-        };
-        golang = {
-          symbol = " ";
-        };
-        hg_branch = {
-          symbol = " ";
-        };
-        hostname = {
-          ssh_symbol = " ";
-        };
-        lua = {
-          symbol = " ";
-        };
-        memory_usage = {
-          symbol = "󰍛 ";
-        };
-        meson = {
-          symbol = "󰔷 ";
-        };
-        nim = {
-          symbol = "󰆥 ";
-        };
-        nix_shell = {
-          symbol = " ";
-        };
-        nodejs = {
-          symbol = " ";
-        };
-        ocaml = {
-          symbol = " ";
-        };
-        package = {
-          symbol = "󰏗 ";
-        };
-        python = {
-          symbol = " ";
-        };
-        rust = {
-          symbol = " ";
-        };
-        swift = {
-          symbol = " ";
-        };
-        zig = {
-          symbol = " ";
-        };
-      };
-    };
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    curl
-    dunst
-    git
-    kitty
-    lazygit
-    neovim
-    networkmanagerapplet # In start.sh 
-    rofi-wayland
-    ripgrep
-    spotify
-    hyprpaper
-    vim
-    (waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-      })
-    )
-    wget
-    whatsapp-for-linux
-    zoxide
-  ];
-
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-  # Desktop portal interactions
-  xdg.portal.enable = true; 
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 }
-
