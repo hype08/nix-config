961ea443db1ffa0f2712e6018ff6840ce201e2c6
hype08 <henry.zhang@hey.com>
2024-11-13 13:16:32 -0800

    remap caps lock

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -109,6 +109,16 @@ in
   security.rtkit.enable = true;
 
   services = {
+    interception-tools = {
+      enable = true;
+      plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+      udevmonConfig = ''
+        - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+          DEVICE:
+            EVENTS:
+              EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+      '';
+    };
     pipewire = {
       alsa = {
         enable = true;
