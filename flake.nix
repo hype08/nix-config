7e2f35e11a766f66e8692b4d718e2a815f25e6a6
hype08 <henry.zhang@hey.com>
2024-11-16 12:04:38 -0800

    wip add fingerprint

--- flake.nix
+++ flake.nix
@@ -9,9 +9,13 @@
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     stylix.url = "github:danth/stylix";
+    nixos-06cb-009a-fingerprint-sensor = {
+      url = "github:ahbnr/nixos-06cb-009a-fingerprint-sensor";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }@inputs: 
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, nixos-06cb-009a-fingerprint-sensor, ... }@inputs: 
     let
       host = "default";
       username = "henry";
@@ -26,6 +30,7 @@
               inherit inputs;
               inherit host;
               inherit username;
+              inherit nixos-06cb-009a-fingerprint-sensor;
           };
           modules = [
             ./hosts/${host}/config.nix
@@ -40,6 +45,9 @@
               };
             }
             inputs.stylix.nixosModules.stylix
+            # https://discourse.nixos.org/t/thinkpad-t480-fingerprint-reader-support/17448/8
+            nixos-06cb-009a-fingerprint-sensor.nixosModules.open-fprintd
+            nixos-06cb-009a-fingerprint-sensor.nixosModules.python-validity
           ];
         };
       };
--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, nixos-06cb-009a-fingerprint-sensor, ... }:
 let
   inherit (import ./variables.nix) keyboardLayout;
 in
@@ -113,6 +113,15 @@ in
   };
 
   security.rtkit.enable = true;
+  services.fprintd = {
+    enable = true;
+    tod = {
+      enable = true;
+      driver = nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
+        calib-data-file = ./calib-data.bin;
+      };
+    };
+  };
 
   services = {
     interception-tools = {
