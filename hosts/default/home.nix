84fc0dea5aaf6f2cc40b1fb4291dad133b1c974a
hype08 <henry.zhang@hey.com>
2024-11-16 00:35:39 -0800

    add systemctl stuff

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -19,6 +19,7 @@ in
     ../../config/hyprland.nix
     ../../config/neovim.nix
     ../../config/waybar.nix
+    ../../config/wlogout.nix
   ];
 
   home.file."Pictures/Wallpapers" = {
@@ -71,6 +72,39 @@ in
       enable = true;
       userName = "${gitUsername}";
       userEmail = "${gitEmail}";
+    };
+    hyprlock = {
+      enable = true;
+      settings = {
+        general = {
+          disable_loading_bar = true;
+          grace = 10;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+        background = [
+          {
+            path = "/home/${username}/Pictures/Wallpapers/wallpaper.jpg";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+        input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            fade_on_empty = false;
+            font_color = "rgb(CFE6F4)";
+            inner_color = "rgb(657DC2)";
+            outer_color = "rgb(0D0E15)";
+            outline_thickness = 5;
+            placeholder_text = "Password...";
+            shadow_passes = 2;
+          }
+        ];
+      };
     };
     kitty = {
       enable = true;
